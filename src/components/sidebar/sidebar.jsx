719176fd2f60d7a1665e7bd3a832f011ab2e9e5a
Oru <tejirioru@gmail.com>
2025-06-18 22:05:59 +0100

    working on the navbar, trying to get appropiate icons and font size

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+        import { FaHome } from 'react-icons/fa';
 import './sidebar.css';
 
 const sidebar = () => {
   return (
     <div>
         <div className='sidebar-content'>
-            <h2>Home</h2>
-            <h2>Assets</h2>
-            <h2>Transactions</h2>
-            <h2>Settings</h2>
-            <h2>Logout</h2>
-            <button className="login">Login</button>
-            <button className="signUp">Signup</button>
+
+            <div className='Navbar'>
+                <h3>Home</h3>
+                <h3>Assets</h3>
+                <h3>Transactions</h3>
+                <h3>Support</h3>
+                <h3>Logout</h3>
+            </div>
+            
+            <div className='Auth'>
+                <button className="login">Login</button>
+                <button className="signUp">Signup</button>
+                <button className="guides">guides</button>
+                <FaHome width={50} height={50}/>
+<span>Home</span>
+
+            </div>
+
         </div>
         
     </div>
