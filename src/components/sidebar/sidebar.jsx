9504dcefd444948d83f666f162da02f1295fad67
Oru <tejirioru@gmail.com>
2025-06-23 22:18:29 +0100

    working on the sidebar menu, trying to get appropiate fonts and colors

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
-        import { FaHome } from 'react-icons/fa';
+import Home from '../../images/home.png'
+import Assets from '../../images/assets.png'
+import Transactions from '../../images/transactions.png'
+import Support from '../../images/support.png'
 import './sidebar.css';
 
 const sidebar = () => {
   return (
     <div>
         <div className='sidebar-content'>
+            
+            <div className='entsidebar'>
 
             <div className='Navbar'>
-                <h3>Home</h3>
-                <h3>Assets</h3>
-                <h3>Transactions</h3>
-                <h3>Support</h3>
-                <h3>Logout</h3>
+                <div className='Home'>
+                    <img src={Home} alt="Home" width={20} height={20}/>
+                    <h3>Home</h3>
+                </div>
+                <div className='Assets'>
+                    <img src={Assets} alt="Assets" width={20} height={20}/>
+                    <h3>Assets</h3>
+                </div>
+                <div className='Transactions'>
+                    <img src={Transactions} alt="Transactions" width={20} height={20}/>
+                    <h3>Transactions</h3>
+                </div>
+                <div className='Support'>
+                    <img src={Support} alt="Support" width={20} height={20}/>
+                    <h3>Support</h3>    
+                </div>
             </div>
-            
+
             <div className='Auth'>
                 <button className="login">Login</button>
                 <button className="signUp">Signup</button>
                 <button className="guides">guides</button>
-                <FaHome width={50} height={50}/>
-<span>Home</span>
-
             </div>
+            
+            </div>
+
 
         </div>
         
